e6b1916a0dff5b9a84df447b9dc3fe411d2fc58a
Ishika Dubey <ishikadubey2020@gmail.com>
2026-07-07 14:00:34 +0530

    CASE STUDY 1

---
+++ script.js
@@ -0,0 +1,12 @@
+function greet(){
+    alert("Welcome to SIT NAGPUR");
+
+    console.table([
+        {
+            College: "SIT Nagpur",
+            Course: "Computer Science"
+        }
+    ]);
+    console.time("Execution");
+    console.timeEnd("Execution");
+}
